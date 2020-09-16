7eb030393d576dd624a393d8612b6ee8c0de24e7
Alex Gim <areygim@gmail.com>
2020-09-16 15:07:13 +0900

    [DESSERTS-32] Cocoapods 세팅

--- Desserts/Desserts/Views/WeeklyViewController.swift
+++ Desserts/Desserts/Views/WeeklyViewController.swift
@@ -14,11 +14,15 @@ class WeeklyViewController: UIViewController {
     @IBOutlet weak var commentLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 하단 string은 API에서 받아오는 것으로 교체될 예정
         let attributedString = NSMutableAttributedString(string: "요 며칠\n우울해 보였는데 😢\n오늘은 기분이 좋아 보여서\n다행이에요. ☺️", attributes: [
           .font: UIFont(name: "SpoqaHanSans-Bold", size: 36.0)!,
           .foregroundColor: UIColor(white: 0.0, alpha: 1.0)
         ])
         commentLabel.attributedText = attributedString
+        
+        
     }
 
     
